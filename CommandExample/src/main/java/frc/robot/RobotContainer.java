64f49b05ed75d2a48a869f621e4a65e7217d8379
Mr. Z <bczdog@gmail.com>
2021-10-07 19:20:22 -0400

    Added MrZPrintCommand and sequential command to run them all.

--- CommandExample/src/main/java/frc/robot/RobotContainer.java
+++ CommandExample/src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.MrZPrintCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -43,6 +45,9 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    //return m_autoCommand;
+    SequentialCommandGroup printSequence = new SequentialCommandGroup();
+    printSequence.addCommands(new MrZPrintCommand());
+    return printSequence;
   }
 }
