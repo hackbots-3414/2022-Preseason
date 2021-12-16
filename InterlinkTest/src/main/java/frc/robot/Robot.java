3f03e694ecc244d767f331265ee80714ed7365da
Ben <bczdog@gmail.com>
2021-12-15 20:19:06 -0500

    Fixed a couple warnings.

--- InterlinkTest/src/main/java/frc/robot/Robot.java
+++ InterlinkTest/src/main/java/frc/robot/Robot.java
@@ -8,8 +8,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMSparkMax;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
